5f7c1a1d3113a67a02842aee96cc7b56a4c2821f
mchmielarz <michal.chmielarz@gmail.com>
2014-09-25 22:11:26 +0200

    Using Fongo instead of embedded Mongo.

--- src/main/groovy/pl/warsjawa/microservice/config/DbConfiguration.groovy
+++ src/main/groovy/pl/warsjawa/microservice/config/DbConfiguration.groovy
@@ -1,15 +1,12 @@
 package pl.warsjawa.microservice.config
 
+import com.github.fakemongo.Fongo
 import com.mongodb.Mongo
-import com.mongodb.WriteConcern
 import groovy.transform.TypeChecked
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.MongoDbFactory
-import org.springframework.data.mongodb.core.MongoFactoryBean
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory
 import org.springframework.data.mongodb.core.WriteResultChecking
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
@@ -19,19 +16,13 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 class DbConfiguration {
 
     @Bean
-    MongoFactoryBean mongo(@Value('${mongodb.host:localhost}') String host,
-                           @Value('${mongodb.port:27017}') int port) {
-        return new MongoFactoryBean(host: host, port: port, writeConcern: WriteConcern.FSYNCED)
+    Mongo mongo() {
+        return new Fongo("marketing-service-fongo").getMongo()
     }
 
     @Bean
-    MongoDbFactory mongoDbFactory(Mongo mongo, @Value('${mongodb.dbname:client}') String dbName) {
-        return new SimpleMongoDbFactory(mongo, dbName)
-    }
-
-    @Bean
-    MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory) {
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory)
+    MongoTemplate mongoTemplate(Mongo mongo, @Value('${mongodb.dbname:client}') String dbName) {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo, dbName)
         mongoTemplate.writeResultChecking = WriteResultChecking.EXCEPTION
         return mongoTemplate
     }
